114714c4fb3f0c960752c3dca6643e132d8f9596
karen <karenlohannyyy@gmail.com>
2025-11-26 20:31:36 -0300

    refactor: alteração da estrutura identificação tailwind

---
+++ src/screens/login.tsx
@@ -0,0 +1,9 @@
+import { Text,  View } from 'react-native';
+
+export function LoginScreen() { 
+    return ( 
+    <View className = "flex-1 items-center justify-center bg-black"> 
+        <Text className="text-6xl text-purple-500 font-bold"> TELA DE LOGIN </Text>
+    </View>
+); 
+}
