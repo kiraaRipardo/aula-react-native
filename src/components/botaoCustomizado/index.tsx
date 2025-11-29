53d13e04d97ee0947dc7ab042c97c56ea7a5927e
karen <karenlohannyyy@gmail.com>
2025-11-28 21:01:28 -0300

    feature: customização da borda e do botão

--- src/components/botaoCustomizado/index.tsx
+++ src/components/botaoCustomizado/index.tsx
@@ -1,7 +1,10 @@
-import {Button, ButtonProps} from 'react-native';
+import {Button, ButtonProps, View} from 'react-native';
 
 export function BotaoCustomizado({...rest}: ButtonProps) {
     return (
+        
+        <View className="rounded-2xl w-full" >
         <Button {...rest}/>
+        </View>
     );
 }
