37d5b4b6305ba4fb094666f8510213231abd67b6
karen <karenlohannyyy@gmail.com>
2025-12-02 20:46:37 -0300

    feature: add variaveis (de login e senha), ocultar senha e capturar info

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,18 +1,25 @@
 import { BotaoCustomizado } from '@/components/botaoCustomizado';
 import { InputCustomizado } from '@/components/InputCustomizado';
-import { Text,  View } from 'react-native';
+import { useState } from 'react';
+import { Text, View } from 'react-native';
 
-export function LoginScreen() { 
-    return ( 
-    <View className = "flex-1 items-center justify-center bg-gray-200"> 
-    <View className='w-full p-5 gap-5 items-center justify-center'>
-    <Text className='text-3xl '>Login do App</Text>
+export function LoginScreen() {
+  const [Login, setLogin, ] = useState('');
+  const [Senha, setSenha] = useState('');
 
-        <InputCustomizado placeholder="login"/>
-        <InputCustomizado placeholder="senha"/>
-        <BotaoCustomizado title="Entrar"/>
-        <Text className=' text-blue-400'> Esqueceu a senha? </Text>
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-200">
+    <View className="w-full items-center justify-center gap-5 p-5">
+    <Text className="text-3xl ">Login do App</Text>
+    
+        <InputCustomizado placeholder="login" onChangeText= {(text) => {setLogin(text)}}/>
+        <InputCustomizado placeholder="senha" secureTextEntry={true} onChangeText= {(text) => {setSenha(text)}}/>
+
+        <BotaoCustomizado title="Entrar" onPress={() => console.log("login", Login, "senha", Senha)} />
+
+        <Text className=" text-blue-400"> Esqueceu a senha? </Text>
+      </View>
     </View>
-    </View>
-); 
+  );
 }
+
