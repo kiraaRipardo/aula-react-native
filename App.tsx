6d06e62fb6800a1d70e38a2095d5b81d50e48406
karen <karenlohannyyy@gmail.com>
2025-12-15 20:03:26 -0300

    feature add Soma de 2 valor, Média, ParOuImpar de 1 num, e contar numeros (+ ou -)

--- App.tsx
+++ App.tsx
@@ -1,19 +1,17 @@
 
 import { StatusBar } from 'expo-status-bar';
-
 import './global.css';
 import { LoginScreen } from '@/screens/login';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Soma } from '@/screens/soma';
-import { Keyboard } from 'react-native';
 
 export default function App() {
   return (
     <>
       <SafeAreaView className="flex-1">
         {/* <LoginScreen /> */}
-         <Soma /> 
-       
+         {/* <Soma /> */}
+         
       </SafeAreaView>
 
       <StatusBar style="auto" />
