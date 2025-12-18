321c69e50c0fbfa51656a21e4f327d5043889068
karen <karenlohannyyy@gmail.com>
2025-12-18 19:16:39 -0300

    feat: add navegação com stack

--- App.tsx
+++ App.tsx
@@ -1,21 +1,24 @@
-
 import { StatusBar } from 'expo-status-bar';
-
 import './global.css';
 import { LoginScreen } from '@/screens/login';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Soma } from '@/screens/soma';
-import { Keyboard } from 'react-native';
 import { ListaScreens } from '@/screens/lista';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <>
       <SafeAreaView className="flex-1">
-        {/* <LoginScreen /> */}
-         {/*<Soma />*/ }
-         <ListaScreens />
-       
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Soma" component={Soma} />
+            <Stack.Screen name="Lista" component={ListaScreens} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </SafeAreaView>
 
       <StatusBar style="auto" />
